8b76ff4396eb71767bc8299f65c658b5e7bb5b9e
Roei Boribor <roeiboribor@gmail.com>
2024-02-27 10:08:44 +0800

    broadcasting on backend

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/playground', function () {
+    event(new \App\Events\PlayGroundEvent());
+    return null;
+});
